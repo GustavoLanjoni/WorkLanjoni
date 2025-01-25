638e5a65a786d77e9cf2111684b98912e13dc6c5
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2025-01-24 21:59:48 -0300

    Termiando!

--- js/vagasPostadas.js
+++ js/vagasPostadas.js
@@ -108,5 +108,19 @@ vagasPostadas.forEach((vaga) => {
 });
 localStorage.setItem('vagasUsuario', JSON.stringify(vagasPostadas));
 
+
+function excluirVaga(index) {
+    if (confirm('Tem certeza de que deseja excluir esta vaga?')) {
+        // Remove a vaga pelo índice
+        vagasPostadas.splice(index, 1);
+        // Atualiza o localStorage com as vagas restantes
+        localStorage.setItem('vagasUsuario', JSON.stringify(vagasPostadas));
+        // Re-renderiza as vagas na tela de "Vagas Postadas"
+        renderVagas();
+        alert('Vaga excluída com sucesso!');
+    }
+}
+
+
 // Inicializa a página exibindo as vagas
 renderVagas();
